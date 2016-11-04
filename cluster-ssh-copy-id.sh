d7c4e79047c107ba3a5e514a01b65a8a95662488
Max Xu <xuhuan@live.cn>
2016-11-05 06:20:44 +0800

    [Add] cluster-ssh-copy-id.sh

---
+++ cluster-ssh-copy-id.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Requirements: 
+# 1. all nodes in Cluster has the same username and password
+# 2. All nodes installed sshpass
+# execute in node1
+
+NODESE="node1 node2 node3 ..."
+
+for src in $NODESE;
+do
+	ssh src
+	ssh-keygen -t rsa
+	for dst in $NODESE;
+	do
+		sshpass -p "111111" ssh -o StrictHostKeyChecking=no dst "ssh-copy-id -i ~/.ssh/id_rsa.pub dst"
+	done
+done
