772749ba044210e584732d01b5f4caa6e6b58316
Max Xu <xuhuan@live.cn>
2016-11-06 18:53:44 +0800

    typo

--- to.sh
+++ to.sh
@@ -1,13 +1,9 @@
 #ssh root@120.24.177.49
-#pass Terminal@207:
 
 
 connect(){
     sshpass -p $1 ssh -o StrictHostKeyChecking=no  $2@120.24.177.49
 }
-
-   # sshpass -p "Bukeuscumarket601" ssh -o StrictHostKeyChecking=no  root@120.24.177.49
-
 
 # 1. via /etc/hosts
 # TODO: ssh -o BatchMode=yes
@@ -17,19 +13,3 @@ sshpass -p "111111" ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no  root@$1
 if [ $? -ne 0 ]; then
 	sshpass -p "111111" ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no  root@10.107.$1
 fi
-
-#while getopts "u" arg
-#do
-#	case $arg in
-#		u)
-#			echo "-u:"
-#			if [ $optarg='root' ]; then
-#				connect Terminal@207 root 
-#			elif [ $optarg='max' ]; then
-#				echo "max"
-#			else
-#				connect Terminal@207 yiave
-#			fi
-#		;;
-#	esac
-#done
