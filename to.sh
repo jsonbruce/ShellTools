a24712ea39fbc299479a727de761a8ef4b905049
Max Xu <xuhuan@live.cn>
2016-11-14 14:41:31 +0800

    [Update]to.sh enhance

--- to.sh
+++ to.sh
@@ -5,9 +5,14 @@ connect(){
     sshpass -p $1 ssh -o StrictHostKeyChecking=no  $2@120.24.177.49
 }
 
+if [ "$1" = "aaron" ]; then
+	ssh zhangchengfei@$1
+
+else
 # 1. via /etc/hosts
 # TODO: ssh -o BatchMode=yes
-sshpass -p "111111" ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no  root@$1
+	sshpass -p "111111" ssh -o ConnectTimeout=3 -o StrictHostKeyChecking=no  root@$1
+fi
 
 # 2. general connect function
 if [ $? -ne 0 ]; then
