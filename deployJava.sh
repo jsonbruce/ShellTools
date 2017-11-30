998675303b873c73612e2da1f2a9fe1eb7ae6199
Max Xu <xuhuan@live.cn>
2017-11-30 21:57:17 +0800

    [New Feature] Init mode and Replace mode. more robust.

--- deployJava.sh
+++ deployJava.sh
@@ -2,7 +2,10 @@
 
 JDK_FILE=""
 JDK_HOME=/usr/lib/java
+JAVA_HOME_NAME="JAVA_HOME"
+SHELL_PROFILE=~/.zshrc
 
+# Usage
 if [ -z $1 ]; then
 	echo -e "Usage: deployJava -f jdkfile -d jdkhome"
 	exit 0
@@ -10,7 +13,8 @@ else
 	echo $0 $*
 fi
 
-# read args
+
+# 0. read args
 while getopts "f:d:" arg
 do
 	case $arg in
@@ -21,9 +25,6 @@ do
 			;;
 		d)
 		    JDK_HOME=$OPTARG	
-			if [ ! -d $JDK_HOME ]; then
-				sudo mkdir $JDK_HOME
-			fi
 			echo "d's arg: $OPTARG"
 			;;
 		?)
@@ -33,12 +34,19 @@ do
 	esac
 done
 
-# echo $JDK_FILE ----- $JDK_HOME
 
 # 1. untar
-tar -zxvf $JDK_FILE -C $JDK_HOME
+if [ ! -d $JDK_HOME ]; then
+	sudo mkdir $JDK_HOME
+else
+	# Delete Older JDK Folder
+	rm -rf $JDK_HOME/*
+fi
 
-#2. set environment in /etc/profile or ~/.bashrc
+tar -zxf $JDK_FILE -C $JDK_HOME
+
+
+# 2. set environment in ~/.zshrc [/etc/profile or ~/.bashrc]
 cd $JDK_HOME
 for f in `pwd`/*
 do
@@ -48,30 +56,35 @@ do
 	fi
 done
 
-echo "JAVA_HOME=" $JAVA_HOME
+echo "JAVA_HOME="$JAVA_HOME
 
-echo export JAVA_HOME=$JAVA_HOME >> ~/.bashrc
-echo 'export JRE_HOME=${JAVA_HOME}/jre'  >> ~/.bashrc
-echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> ~/.bashrc
-echo 'export PATH=${JAVA_HOME}/bin:$PATH'  >> ~/.bashrc
-source ~/.bashrc
+if [ -z `grep -q $JAVA_HOME_NAME $SHELL_PROFILE` ]; then
+	echo "Replace JAVA_HOME..."
+	sed -i "s:^export ${JAVA_HOME_NAME}.*:export ${JAVA_HOME_NAME}=${JAVA_HOME}:" $SHELL_PROFILE
+else
+	echo "Init JAVA_HOME..."
+	echo export JAVA_HOME=$JAVA_HOME >> ~/.zshrc
+	echo 'export JRE_HOME=${JAVA_HOME}/jre'  >> ~/.zshrc
+	echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> ~/.zshrc
+	echo 'export PATH=${JAVA_HOME}/bin:$PATH'  >> ~/.zshrc
+fi
 
-# 3. unset openJDK
+source ~/.zshrc
 
+
+# 3. unset openJDK and Older JDK
 update-alternatives --remove-all java
 update-alternatives --remove-all javac
 update-alternatives --remove-all jar
 update-alternatives --remove-all javah
 update-alternatives --remove-all javap
 
+update-alternatives --install /usr/bin/java java $JAVA_HOME/bin/java 1
+update-alternatives --install /usr/bin/javac javac $JAVA_HOME/bin/javac 1
+update-alternatives --install /usr/bin/jar jar $JAVA_HOME/bin/jar 1
+update-alternatives --install /usr/bin/javah javah $JAVA_HOME/bin/javah 1
+update-alternatives --install /usr/bin/javap javap $JAVA_HOME/bin/javap 1
 
-update-alternatives --install /usr/bin/java java $JAVA_HOME/bin/java 300
-update-alternatives --install /usr/bin/javac javac $JAVA_HOME/bin/javac 300
-update-alternatives --install /usr/bin/jar jar $JAVA_HOME/bin/jar 300
-update-alternatives --install /usr/bin/javah javah $JAVA_HOME/bin/javah 300
-update-alternatives --install /usr/bin/javap javap $JAVA_HOME/bin/javap 300
-
-update-alternatives --config java
 
 # 4. complete
-echo "Deploy Completed!"
+echo "Java Deploy Completed!"
