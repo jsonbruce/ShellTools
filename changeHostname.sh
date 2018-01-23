dea2cd7dc85b62fa12cd51a6399f0d34e6fc86c6
Max Xu <xuhuan@live.cn>
2018-01-23 21:53:40 +0800

    Fix bug

--- changeHostname.sh
+++ changeHostname.sh
@@ -5,6 +5,8 @@ rack=`echo $ip | cut -d"." -f3`
 server=`echo $ip | cut -d"." -f4`
 
 nodename="node"${rack}"-"${server}
+oldname=`hostname`
 
 echo $nodename > /etc/hostname
-sed -i "s/^127.0.1.1.*$/127.0.1.1       ${hostname}/" /etc/hosts
+#sed -i "s/^127\.0.*$oldname$/$ip       $nodename/" /etc/hosts
+sed -i -e "s/^127\.0.*$oldname$/#&/" /etc/hosts
