a924d40a4dd804c6560ebd83386e87c1fb9e78b1
Max Xu <xuhuan@live.cn>
2019-09-12 18:51:13 +0800

    Refact clusterDeployFiles script with getopt to support more options

--- clusterDeployFiles.sh
+++ clusterDeployFiles.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #########################################################################
 # --------->    FILE: clusterDeployFiles.sh
 # --------->    AUTHOR: Max Xu
@@ -5,38 +7,104 @@
 # --------->    DATE: 01/22/2018    TIME:19:40:03
 #########################################################################
 
-#!/bin/bash
+ARGS=`getopt -o n:u:p:d:h -l nodes:,user:,port:,dir:,help -- "$@"`
+
+if [ $? != 0 ]; then
+    echo "Terminating..."
+    exit 1
+fi
+
+eval set -- "${ARGS}"
 
 USAGE="
-Usage: $0 node_list_file username port dst_dir file1 [file2 file3 ...]
+Usage: $0 -n|--nodes node_list_file [-u|--user user -p|--port port -d|--dir dst_dir] file1 [file2 file3 ...]
 
 Deploy Files to each node in cluster.
 
 node_list_file                  a file. node name or ip per line
-username                        username of each node
+user                            username of each node
 port                            ssh port of each node(default to 22)
 dst_dir                         directory in node  to be deployed
 file[n]                         file or directory list seperated by blank
-
 "
 
-
-if [ -z $5 ]; then
+function usage() {
 	printf "%s\\n" "$USAGE"
+}
+
+if [ -z $1 ];then
+	usage
 	exit 0
 fi
 
+while true
+do
+	case "$1" in
+		-n|--nodes)
+			#echo "Option -n", $2
+            NODES=`cat $2`
+			shift 2
+			;;
+		-u|--user)
+			case "$2" in
+				"")
+					#echo "Option -u, no arg"
+					U=""
+					shift 2
+					;;
+				*)
+					#echo "Option -u, arg $2"
+					U="$2@"
+					shift 2
+					;;
+			esac
+			;;
+		-p|--port)
+			case "$2" in
+				"")
+					#echo "Option -p, no arg"
+					PORT=""
+					shift 2
+					;;
+				*)
+					#echo "Option -p. arg $2"
+					PORT="-P $2"
+					shift 2
+					;;
+			esac
+			;;
+		-d|--dir)
+			case "$2" in
+				"")
+					#echo "Option -d, no arg"
+					shift 2
+					;;
+				*)
+					#echo "Option -d, arg $2"
+                    DST_DIR=$2
+					shift 2
+					;;
+			esac
+			;;
+		-h|--help)
+			usage
+			exit 0
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "$1 is not an option!"
+			exit 1
+			;;
+	esac
+done
 
 
-NODES=`cat $1`
-USERNAME=$2
-port=$3
-DST_DIR=$4
-shift 4
-FILES="$*"
-
+FILES=$*
 
 for node in $NODES
 do
-	scp -P $port -r $FILES $USERNAME@$node:$DST_DIR
+	scp $PORT -r $FILES $U$node:$DST_DIR
 done
